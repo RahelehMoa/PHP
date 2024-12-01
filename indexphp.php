13c731b1dd302a59b084f00ef226346ca9dee9d6
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-01 13:43:34 +0330

    Add files via upload

---
+++ indexphp.php
@@ -0,0 +1,18 @@
+
+
+<?php
+    echo "I am Raheleh Moallemi"; 
+    echo "<br>";
+    echo "hi good morning";
+    echo "<br>";
+    print "1 December 2024";
+    echo "<br>";
+    echo 100 + 200 ;
+?> 
+<!-- echo is for print of text, number or everythings -->
+
+
+
+
+
+
