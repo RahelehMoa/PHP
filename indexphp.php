3dd15a86b4e800275a2c9dfb0bac2160e00c4c80
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-02 22:06:48 +0330

    Add files via upload

--- indexphp.php
+++ indexphp.php
@@ -8,8 +8,14 @@
     print "1 December 2024";
     echo "<br>";
     echo 100 + 200 ;
+    echo "<br>";
+    $A = "i work on php";
+    echo $A ;
 ?> 
 <!-- echo is for print of text, number or everythings -->
+<!-- $ is a variable that is always used before of putting a variable. -->
+<!-- variables are of A-Z after that we put numbers of 0-9 -->
+
 
 
 
