58d2d28abab571c35111bb6cb8ce6279c3300d02
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-13 21:06:57 +0330

    Add files via upload

--- indexphp.php
+++ indexphp.php
@@ -9,16 +9,22 @@
     echo "<br>";
     echo 100 + 200 ;
     echo "<br>";
-    $A = "i work on php";
+    $A = " <h1>i work on php</h1>";
     echo $A ;
     echo "<br>";
-    $Number = 500;
+    $Number = 500 ;
+    echo $Number ;
+    echo "<br>";
+    $Number = "<h1>1000</h1>" ;
     echo $Number ;
 
 ?> 
 <!-- echo is for print of text, number or everythings -->
 <!-- $ is a sign / mark that is always used before of putting a variable. -->
 <!-- variables are of A-Z after that we put numbers of 0-9 -->
+<!-- Integer or Numbers shch as 50 , 400 shows alone but String and letters shows into " " -->
+<!-- If we use of Tages H1 - H6 then we have to shows into " " -->
+
 
 
 
