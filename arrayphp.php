47c412f4148c0f17ec97220991d6693fd8cb7973
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-14 13:26:05 +0330

    Add files via upload

---
+++ arrayphp.php
@@ -0,0 +1,24 @@
+
+
+<?php
+    echo "I am Raheleh Moallemi"; 
+    echo "<br>";
+    echo "hi good morning";
+    echo "<br>";
+    
+    $boolean = true ;
+    echo $boolean ;
+    echo "<br>";
+    var_dump ($boolean);
+
+?> 
+
+<!-- Data Type is known with var_dump ($variable) in php automatically  -->
+
+
+
+
+
+
+
+
