46d67afe2dc327d1f093dfdcb3b95fda293b5dff
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-14 21:25:12 +0330

    Add files via upload

--- arrayphp.php
+++ arrayphp.php
@@ -1,21 +1,20 @@
 
 
 <?php
-    echo "I am Raheleh Moallemi"; 
-    echo "<br>";
-    echo "hi good morning";
-    echo "<br>";
+
     
-    $boolean = true ;
-    echo $boolean ;
-    echo "<br>";
-    var_dump ($boolean);
+    $arr = array (500 , "i work on php" , 5.5 , true) ;
+     echo $arr [0] ;
+     echo "<br>" ;
+     echo "<br>" ;
+     var_dump ($arr );
+    
 
 ?> 
 
-<!-- Data Type is known with var_dump ($variable) in php automatically  -->
+<!-- each array include of andises that start from 0 , 1 , 2 ,.... we use echo for showing each Andis -->
 
-
+<!-- var_dump ($arr ); that is for reading/ showing a full array  -->
 
 
 
