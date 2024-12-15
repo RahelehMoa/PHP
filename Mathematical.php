d54b70c71535ea11aef8f779084eaf0d8aa53251
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-15 21:43:54 +0330

    Add files via upload

---
+++ Mathematical.php
@@ -0,0 +1,32 @@
+
+
+<?php
+
+    
+$math1 = 100;
+$math2 = 200;
+
+// $math = $math1 + $math2;
+// $math = $math1 * $math2;
+// $math = $math1 / $math2;
+// $math = $math1 - $math2;
+// echo $math;
+
+$i = 10;
+// $i++ ; 
+// $i-- ;
+// echo $i;
+   
+    
+
+?> 
+
+<!-- the Maths also show with variables -->
+
+<!--  -->
+
+
+
+
+
+
