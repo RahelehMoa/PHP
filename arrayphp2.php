cda902c57da10011079ac2d1ceb128b4f128bc2b
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-15 19:39:10 +0330

    Add files via upload

---
+++ arrayphp2.php
@@ -0,0 +1,20 @@
+
+
+<?php
+
+    
+    $arr = array ("") ;
+   
+    
+
+?> 
+
+<!-- each array include of andises that start from 0 , 1 , 2 ,.... we use echo for showing each Andis -->
+
+<!-- var_dump ($arr ); that is for reading/ showing a full array  -->
+
+
+
+
+
+
