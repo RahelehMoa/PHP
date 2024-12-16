7ed825ae5b67c3a32a5b81bf214bd4fe3b14e3ce
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-16 17:58:38 +0330

    Add files via upload

---
+++ IF and Else.php
@@ -0,0 +1,26 @@
+
+
+<?php
+
+    
+$math1 = 100;
+$math2 = 200;
+
+// $math = $math1 + $math2;
+
+
+echo "<h1 style = 'color: green;'>" . $name . " " . $lastname . "</h1>" ; 
+echo "<h1 style = 'color: red;'>  $name" . " " . "$lastname  </h1>" ; 
+
+
+    
+
+?> 
+
+
+
+
+
+
+
+
