94b9debcb4cee2e8344ccdc113519bf9e31cf7c3
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-16 19:53:00 +0330

    Add files via upload

--- IF and Else.php
+++ IF and Else.php
@@ -14,19 +14,27 @@ The desired commands if the condition is met.
 
 */
 
-// == that is equal.
-// && that is and.
+// == that is Equal.
+// && that is And.
 // || that is or.
+// != that is not Equal
 
 $user = "Raheleh";
 $pass = "123";
+$number = 10;
 
 //  if ($user == "Raheleh" && $pass == "123")
 //  if ($user == "Raheleh" || $pass == "1234")
 //  if ($user == "Raheleh" || $user == "Amir")
+//  if ($user != "Raheleh")
+//  if ($user != "Amir")
+//  if ($number > 10)
+//  if ($number >= 10)
+//  if ($number <= 10)
 
-if ($user == "Raheleh" || $user == "Amir")
 
+
+if ($number <> 10)
 {
 
 echo "yes";
