01573e5dd98b304a0a4f2c72f3f100e5e3197e37
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2024-12-18 19:20:07 +0330

    Add files via upload

--- Foreach.php
+++ Foreach.php
@@ -2,12 +2,18 @@
 
 <?php
 
+ $names = array ("Raheleh " , " Soroush " , " Sina ", " Amir ") ;
+
+ foreach ($names as $item) {
+  
+  echo "<option value = \"1000\"> $item </option>";
+
+
+ }
  
     
 
 ?> 
-
-
 
 
 
